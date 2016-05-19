3bc4796a1a72690ecbfaba530fb1783379dc2625
Bruno Basto <bruno.basto@liferay.com>
2016-05-19 14:30:55 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,92 +1,37 @@
 'use strict';
 
-var gulp = require('gulp');
-var karma = require('karma').server;
-var merge = require('merge');
-var openFile = require('open');
-var path = require('path');
+var fs = require('fs');
+var properties = require('properties');
 
-module.exports = function(config) {
-	gulp.task(
-		'test:unit',
-		[],
-		function(done) {
-			runKarma(config, {}, done);
-		}
-	);
+var Properties = {
+	preprocess: function(contents) {
+		return contents.replace(/\s*#.*/g, '');
+	},
 
-	gulp.task(
-		'test:unit:coverage',
-		[],
-		function(done) {
-			runKarma(
-				config,
-				{
-					configFile: path.resolve(config.karmaCoverageFile),
-					coverage: true
-				},
-				function() {
-					done();
+	read: function(fileName, callback) {
+		var instance = this;
+
+		var contents = fs.readFileSync(
+			fileName,
+			{
+				encoding: 'utf8'
+			}
+		);
+
+		properties.parse(
+			instance.preprocess(contents),
+			{
+				path: false
+			},
+			function(error, obj) {
+				if (error) {
+					return error;
 				}
-			);
-		}
-	);
 
-	gulp.task(
-		'test:unit:coverage:open',
-		['test:unit:coverage'],
-		function(done) {
-			openFile(path.resolve(config.karmaCoverageOutput + '/lcov/lcov-report/index.html'));
-			done();
-		}
-	);
-
-	gulp.task(
-		'test:unit:browsers',
-		[],
-		function(done) {
-			runKarma(
-				config,
-				{
-					browsers: ['Chrome', 'Firefox', 'Safari']
-				},
-				done
-			);
-		}
-	);
-
-	gulp.task(
-		'test:unit:watch',
-		[],
-		function(done) {
-			runKarma(
-				config,
-				{
-					singleRun: false
-				},
-				done
-			);
-		}
-	);
+				return callback.call(instance, [obj]);
+			}
+		);
+	}
 };
 
-function runKarma(config, karmaConfig, done) {
-	karmaConfig = merge(
-		{
-			configFile: path.resolve(config.karmaFile),
-			singleRun: true
-		},
-		karmaConfig
-	);
-
-	karma.start(
-		karmaConfig,
-		function(exitCode) {
-			if (exitCode) {
-				console.log('Tests failed.');
-			}
-
-			done();
-		}
-	);
-}
+module.exports = Properties;
